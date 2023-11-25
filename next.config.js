da6f58b614bc2140534e3c281f37db04e1b2fa6c
sajadevo <nawabisajadahmad@gmail.com>
2023-11-25 18:20:54 +0430

    update the app

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -10,8 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  assetPrefix: "/nextjs-tailwind-blog-post-details-page/",
-  basePath: "/nextjs-tailwind-blog-post-details-page",
 };
 
 module.exports = nextConfig;
